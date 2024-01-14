e8882884dae7e37dbe8e9f91d9f8f517996b6b40
Abhishek Kumar <ak3037308@gmail.com>
2024-01-14 12:41:50 +0530

    first commit

---
+++ app/edit_work/page.jsx
@@ -0,0 +1,16 @@
+'use client'
+
+
+import Form from '@components/Form'
+import React, { useState } from 'react'
+
+const EditWork = () => {
+  
+  return (
+    <>
+    <Form type="edit"/>
+    </>
+  )
+}
+
+export default EditWork
